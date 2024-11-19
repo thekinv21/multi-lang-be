2dfeffab7c6ceea3e5fc31c0abc9b08077acb878
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-19 22:41:54 +0300

    first commit

---
+++ src/main/java/com/MultiLanguage/MultiLanguage/MultiLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.MultiLanguage.MultiLanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiLanguageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MultiLanguage/MultiLanguage/MultiLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MultiLanguage.MultiLanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
